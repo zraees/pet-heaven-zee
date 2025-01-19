2c51f0d19c82f8d69fbac33482da2d17656b7d43
Ali <ali.subhani@tintash.com>
2025-01-19 23:36:18 +0200

    Add tailwinf configurations for tthe entire project

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        fieldwork: ["Fieldwork", "sans-serif"],
+      },
+      lineHeight: {
+        110: "1.1",
+        120: "1.2",
+      },
+      boxShadow: {
+        "3xl": "0px 0px 5px 0px #0000001A",
+      },
+    },
+    colors: {
+      white: {
+        0: "#ffffff",
+      },
+      black: {
+        0: "#000000",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+    },
+  },
+  plugins: [],
+};
