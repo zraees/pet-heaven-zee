13e355a985876730bc6bbfc939ef619274d28876
Ali <ali.subhani@tintash.com>
2025-01-20 13:19:49 +0200

    Add bottom banner

---
+++ src/Pages/Home/BottomBanner.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import WomanWithPet from '../../Assets/WomanWithPet.svg'
+
+const BottomBanner: React.FC = () => {
+  return (
+    <section className="flex items-center justify-center pt-2 pb-10">
+      <div className="bg-yellow-0 rounded-lg flex sm:flex-row flex-col items-center px-8 p-2 w-[80%] mx-auto">
+        <div className="w-[90%] mx-auto sm:w-1/2">
+          <img
+            src={WomanWithPet}
+            alt="Woman"
+            className="w-[400px] h-auto"
+          />
+        </div>
+
+        <div className="w-[90%] mx-auto sm:w-1/2 px-6 text-center sm:text-right">
+          <h2 className="text-3xl font-bold text-black">One More Friend</h2>
+          <h3 className="text-2xl font-bold text-black mt-2">
+            Thousands More Fun!
+          </h3>
+          <p className="text-gray-800 mt-4">
+            Having a pet means you have more joy, a new friend, a happy person
+            who will always be with you to have fun. We have 200+ different pets
+            that can meet your needs!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BottomBanner;
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react';
 import TopBanner from './TopBanner.tsx';
 import PetList from './PetList.tsx';
+import BottomBanner from './BottomBanner.tsx';
 
 const Home: FC = () => {
     return (
         <div>
             <TopBanner />
             <PetList />
+            <BottomBanner />
         </div>
     )
 }
