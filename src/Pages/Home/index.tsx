10e4f3fb8e10ba1fb237859db0c2dec7540e2f56
Ali <ali.subhani@tintash.com>
2025-01-20 13:09:01 +0200

    Update home page to show list of pets

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import TopBanner from './TopBanner.tsx';
+import PetList from './PetList.tsx';
 
 const Home: FC = () => {
     return (
         <div>
             <TopBanner />
+            <PetList />
         </div>
     )
 }
