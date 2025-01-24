432533db5ee16c78d02b8ca0ebe611714244ec08
Ali <ali.subhani@tintash.com>
2025-01-24 23:13:51 +0200

    Add green color to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-        fieldwork: ["Fieldwork", "sans-serif"],
+        roboto: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        fieldwork: ['Fieldwork', 'sans-serif'],
       },
       lineHeight: {
-        110: "1.1",
-        120: "1.2",
+        110: '1.1',
+        120: '1.2',
       },
       boxShadow: {
-        "3xl": "0px 0px 5px 0px #0000001A",
+        '3xl': '0px 0px 5px 0px #0000001A',
       },
     },
     colors: {
       white: {
-        0: "#ffffff",
-        1: "F5F5F5",
+        0: '#ffffff',
+        1: 'F5F5F5',
+      },
+      green: {
+        0: '#2E9C3F',
       },
       red: {
-        0: "#D81B39",
+        0: '#D81B39',
       },
       yellow: {
-        0: "#F7A009",
+        0: '#F7A009',
       },
       black: {
-        0: "#000000",
+        0: '#000000',
       },
     },
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      xxl: "1536px",
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px',
     },
   },
   plugins: [],
