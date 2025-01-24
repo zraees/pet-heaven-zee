be4fd42bd87af9d4d831c3d92da27cba8591d044
Ali <ali.subhani@tintash.com>
2025-01-25 01:47:04 +0200

    add animation effect to customer list and also adjust its mobile layout

--- src/Pages/PetDetails/CustomerList.tsx
+++ src/Pages/PetDetails/CustomerList.tsx
@@ -1,34 +1,45 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../../Store/index.ts";
-import { fetchCustomers } from "../../Reducers/petReducer.tsx";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../Store/index.ts';
+import { fetchCustomers } from '../../Reducers/petReducer.tsx';
+import { Slide } from 'react-awesome-reveal';
 
 const CustomerList: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { customers, customersStatus, error } = useSelector((state: RootState) => state.pets);
+  const { customers, customersStatus, error } = useSelector(
+    (state: RootState) => state.pets
+  );
 
   useEffect(() => {
-    if (customersStatus === "idle") {
+    if (customersStatus === 'idle') {
       dispatch(fetchCustomers());
     }
   }, [dispatch, customersStatus]);
 
   return (
     <section className="max-w-6xl mx-auto py-10">
-      <h2 className="text-2xl font-bold mb-6 sm:text-left text-center">Customer Images</h2>
+      <h2 className="text-2xl font-bold mb-6 sm:text-left text-center">
+        Customer Images
+      </h2>
 
-      {customersStatus === "loading" && <p className="text-center">Loading customers...</p>}
-      {customersStatus === "failed" && <p className="text-red-0 text-center">{error}</p>}
+      {customersStatus === 'loading' && (
+        <p className="text-center">Loading customers...</p>
+      )}
+      {customersStatus === 'failed' && (
+        <p className="text-red-0 text-center">{error}</p>
+      )}
 
-      {customersStatus === "succeeded" && customers?.length > 0 ? (
+      {customersStatus === 'succeeded' && customers?.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {customers.map((customer) => (
-            <div key={customer.id} className="rounded-lg overflow-hidden shadow-md">
-              <img
-                src={customer.customer_image}
-                alt={`Customer ${customer.id}`}
-                className="w-full h-96 object-cover rounded-lg"
-              />
+            <div key={customer.id} className="rounded-lg overflow-hidden">
+              <Slide direction="left">
+                <img
+                  src={customer.customer_image}
+                  alt={`Customer ${customer.id}`}
+                  className="w-[85%] mx-auto sm:w-full h-96 object-cover rounded-lg shadow-md"
+                />
+              </Slide>
             </div>
           ))}
         </div>
