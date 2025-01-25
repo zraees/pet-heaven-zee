fb116c4c0135a1e401f67ba0cc258e37d28b80fa
Ali <ali.subhani@tintash.com>
2025-01-25 02:47:48 +0200

    add menu optons details to constant for using it in mobile navbar

--- src/Utils/constants/index.tsx
+++ src/Utils/constants/index.tsx
@@ -8,3 +8,9 @@ export enum Navigation {
 
 export const BASE_URL =
   'https://d0d70d41-858e-4b30-89b0-3b6a3dd32625.mock.pstmn.io/';
+
+
+export const navbarItems = [
+    {title: 'Home', path: '/'},
+    {title: 'Contact', path: '/contact'},
+]
